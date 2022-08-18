0ceed47cb0c50ee28c29732bba76a60eeb4e379c
Vable25 <artur.metkov@mail.ru>
2022-08-18 14:49:10 +0300

    Core1

---
+++ pass.js
@@ -0,0 +1,25 @@
+/*
+window.location.href = window.location.origin + "/en/"
+history.pushState(null, null, '/en');
+history.replaceState(stateObj, title, [url])
+Console.log(location.href)
+
+let stateObj = { foo: "bar" }
+history.pushState(stateObj, "page 2", "bar.html")
+history.replaceState(stateObj, "page 3", "bar.html")
+
+if (document.location.reload) 
+document.location.replace('vable555.github.io')
+
+
+<link rel="alternate" hreflang="ru" href="https://context.reverso.net/%D0%BF%D0%B5%D1%80%D0%B5%D0%B2%D0%BE%D0%B4/">
+<link rel="alternate" hreflang="en" href="https://context.reverso.net/translation/">
+<link rel="alternate" hreflang="fr" href="https://context.reverso.net/traduction/">
+
+<a href="/translation/" class="option" data-value="en" data-default-size="14px">English</a>
+
+*/
+
+
+
+
